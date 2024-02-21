a57e4bca7522a99618d162d657c823c72f3bba6d
Duat Tran <duatsen36@gmail.com>
2024-02-22 00:35:39 +0700

    create class Entities

---
+++ src/server/FarmProduce.Core/Entities/Products.cs
@@ -0,0 +1,15 @@
+﻿using FarmProduce.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmProduce.Core.Entities
+{
+	public class Products :IEntity
+	{
+		public int Id { get; set; }
+
+	}
+}
