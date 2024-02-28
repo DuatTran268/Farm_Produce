44c3bbab53fa1550518edd07d6662f0f3017fc22
Duat Tran <duatsen36@gmail.com>
2024-02-28 22:46:48 +0700

    commit to branch switch

--- src/server/FarmProduce.Data/Contexts/FarmDbContext.cs
+++ src/server/FarmProduce.Data/Contexts/FarmDbContext.cs
@@ -35,10 +35,10 @@ namespace FarmProduce.Data.Contexts
 
 		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server=XUANHUNG\\SQLEXPRESS;Database=FarmProducts;Trusted_Connection=True;MultipleActiveResultSets=true;TrustServerCertificate=True");
-			//optionsBuilder.UseSqlServer("Server=DESKTOP-NLUPE1I\\MSSQLSERVER01;Database=FarmProduct;Trusted_Connection=True;MultipleActiveResultSets=true;TrustServerCertificate=True");
+            //optionsBuilder.UseSqlServer("Server=XUANHUNG\\SQLEXPRESS;Database=FarmProducts;Trusted_Connection=True;MultipleActiveResultSets=true;TrustServerCertificate=True");
+            optionsBuilder.UseSqlServer("Server=DESKTOP-NLUPE1I\\MSSQLSERVER01;Database=FarmProduct;Trusted_Connection=True;MultipleActiveResultSets=true;TrustServerCertificate=True");
 
-		}
+        }
        
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
