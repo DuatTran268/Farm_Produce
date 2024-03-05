b7d9e3d2277ebcbe2cbd85e7149f5bd442b89c0c
Duat Tran <duatsen36@gmail.com>
2024-03-05 22:43:57 +0700

    add file

---
+++ src/server/FarmProduct.WebApi/Models/Categories/CategoriesDetail.cs
@@ -0,0 +1,9 @@
+﻿namespace FarmProduct.WebApi.Models.Categories
+{
+	public class CategoriesDetail
+	{
+        public int Id { get; set; }
+		public string Name { get; set; }
+        public string UrlSlug { get; set; }
+    }
+}
---
+++ src/server/FarmProduct.WebApi/Models/Products/ProductsDto.cs
@@ -0,0 +1,16 @@
+﻿namespace FarmProduct.WebApi.Models.Products
+{
+	public class ProductsDto
+	{
+		public int Id { get; set; }
+        public string Name { get; set; }
+        public string UrlSlug { get; set; }
+        public int QuantityAvailable { get; set; }
+        public string Unit { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public DateTime DateCreate { get; set; }
+        public DateTime DateUpdate { get; set; }
+        public bool Status { get; set; }
+    }
+}
