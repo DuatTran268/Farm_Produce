9e92915c220522d2b75b4a640cffe46241486fc4
Duat Tran <duatsen36@gmail.com>
2024-03-08 00:30:07 +0700

    Get API: get list product and detail product by slug

--- src/client/src/api/Product.js
+++ src/client/src/api/Product.js
@@ -1,8 +1,16 @@
 import { get_api } from "./AxiosCommon";
 
+export async function getAllProduct(){
+  return get_api (`https://localhost:7047/api/products/getall`)
 
+}
 
 export async function getProductNewestLimit(){
   return get_api (`https://localhost:7047/api/products/limitNewest/4`)
 
 }
+
+export async function getDetailProductByUrlSlug(urlSlug = ''){
+  return get_api (`https://localhost:7047/api/products/slugProduct/${urlSlug}`)
+}
+
--- src/client/src/components/user/product/ProductDetails.jsx
+++ src/client/src/components/user/product/ProductDetails.jsx
@@ -1,53 +1,67 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
 import "../../../styles/user/ProductDetail.css";
 import dautay from "../../../assets/mutdau.jpg";
 import { Image } from "react-bootstrap";
+import { getDetailProductByUrlSlug } from "../../../api/Product";
 
 const ProductDetails = () => {
+  const params = useParams();
+  const [productDetail, setProductDetail] = useState(null);
 
 
-  
-  return (
-    <section>
-      <div className="product_detail">
-        <div className="product_detail_img col-5">
-          <Image src={dautay} width={300} />
-        </div>
-        <div className="product_detail_content col-7">
-          <div className="product_detail_title">Dâu tây nhật bản</div>
-          <div className="product_detail_price">175.000đ</div>
-          <div className="product_detail_desc">
-            Dâu tây Nhật Bản cho quả đỏ, mọng nước, hương thơm mùi kẹo ngọt và
-            có vị ngọt thanh đậm đà, khác với tất cả các loại dâu khác đang được
-            trồng tại Đà Lạt và vùng lân cận hiện nay. Có các lựa chọn về size
-            dâu dành cho quý khách: Quy cách đóng hộp: 500gr/hộp Size 25
-            trái/hộp; 30 trái; 36 trái; 49 trái và 64 trái/hộp Quý khách NÊN đặt
-            trước ít nhất 01 ngày để DaLaVi lên đơn, lên lịch hái ngày hôm sau
-            và chuyển đi từ Đà Lạt nhằm đảm bảo sự TƯƠI NGON. Tư vấn và đặt
-            hàng: 0914107107
+  const {slug} = params;
+
+  useEffect(() => {
+    document.title ="Chi tiết sản phẩm";
+    getDetailProductByUrlSlug(slug).then((data) => {
+      if (data){
+        setProductDetail(data);
+
+      }
+      else{
+        setProductDetail({});
+      }
+    });
+  }, [slug])
+
+  if (productDetail){
+    return (
+      <section>
+        <div className="product_detail">
+          <div className="product_detail_img col-5">
+            <Image src={dautay} width={300} />
           </div>
-          <div className="product_detail_size">
-            <label for="cars">Lựa chọn size: </label>
-            <select name="cars" id="cars">
-              <option value="volvo">S</option>
-              <option value="saab">M</option>
-              <option value="opel">L</option>
-              <option value="audi">XL</option>
-            </select>
-          </div>
-          <div className="product_detail_quantity_cart">
-            <div className="product_detail_quantity">
-              <span>Số lượng</span>
-              <input type="number" min="1" max="100" />
+          <div className="product_detail_content col-7">
+            <div className="product_detail_title">{productDetail.name}</div>
+            <div className="product_detail_price">{productDetail.price}</div>
+            <div className="product_detail_desc">
+              {productDetail.description}
             </div>
-            <Link className="btn btn-success product_detail_addcart" to={'/cart'}>
-              Thêm vào giỏ hàng
-            </Link>
+            <div className="product_detail_size">
+              <label for="cars">Lựa chọn size: </label>
+              <select name="cars" id="cars">
+                <option value="volvo">S</option>
+                <option value="saab">M</option>
+                <option value="opel">L</option>
+                <option value="audi">XL</option>
+              </select>
+            </div>
+            <div className="product_detail_quantity_cart">
+              <div className="product_detail_quantity">
+                <span>Số lượng</span>
+                <input type="number" min="1" max="100" />
+              </div>
+              <Link className="btn btn-success product_detail_addcart" to={'/cart'}>
+                Thêm vào giỏ hàng
+              </Link>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
-  );
+      </section>
+    );
+
+  }
+
 };
 export default ProductDetails;
